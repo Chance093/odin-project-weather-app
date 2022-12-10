7b5e469646fae9b8c9f00414b9962efe70e04363
Chance093 <chance.dev093@gmail.com>
2022-12-09 20:37:36 -0800

    Add function getCurrentHighTemp()

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import { currentWeatherData } from "./json/current-example";
 function getCurrentTemp(data){
     return Math.round(data.main.temp);
 }
+
+function getCurrentHighTemp(data){
+    return Math.round(data.main.temp_max) + 2;
+}
+
